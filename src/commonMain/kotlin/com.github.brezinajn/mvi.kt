dcf88d39a8dce478e013c9a5feb0a8ca2b5daf72
Jan Březina <brezina.jn@gmail.com>
2022-04-13 14:29:27 +0200

    Initial commit

---
+++ src/commonMain/kotlin/com.github.brezinajn/mvi.kt
@@ -0,0 +1,55 @@
+package com.github.brezinajn
+
+import arrow.core.Either
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+
+typealias Reducer<ACTION, STATE> = (ACTION, STATE) -> STATE
+typealias SideEffect<ACTION, STATE, ERROR> = suspend (ACTION, STATE, Dispatch<ACTION>) -> Either<ERROR, Unit>
+typealias Dispatch<ACTION> = (ACTION) -> Unit
+
+fun <ACTION, STATE> MVI(
+    setState: (STATE) -> Unit,
+    getState: () -> STATE,
+    reducer: Reducer<ACTION, STATE>,
+    sideEffect: ((ACTION, STATE, Dispatch<ACTION>) -> Unit)? = null,
+): Dispatch<ACTION> {
+    fun dispatch(action: ACTION) {
+        val newState = reducer(action, getState())
+        setState(newState)
+        sideEffect?.invoke(action, newState, ::dispatch)
+    }
+
+    return ::dispatch
+}
+
+fun <ACTION, STATE, ERROR> MVI(
+    getState: () -> STATE,
+    setState: (STATE) -> Unit,
+    reducer: Reducer<ACTION, STATE>,
+    coroutineScope: CoroutineScope,
+    sideEffect: SideEffect<ACTION, STATE, ERROR>,
+    onError: suspend (ACTION, STATE, ERROR) -> Unit,
+    onThrowable: suspend (ACTION, STATE, Throwable) -> Unit,
+    logAction: (suspend (ACTION, STATE) -> Unit)? = null,
+): Dispatch<ACTION> = MVI(
+    getState = getState,
+    setState = setState,
+    reducer = reducer,
+    sideEffect = { action, newState, dispatch ->
+
+        if (logAction != null) {
+            coroutineScope.launch {
+                logAction(action, newState)
+            }
+        }
+
+        coroutineScope.launch {
+            Either.catch {
+                sideEffect(action, newState, dispatch)
+                    .tapLeft { onError(action, newState, it) }
+            }.tapLeft { onThrowable(action, newState, it) }
+        }
+    }
+)
